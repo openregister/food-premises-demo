3ee13ee755db2e964237bef0954b663f1d9f497b
Adam Shimali <adam.shimali@gmail.com>
2015-07-13 22:35:06 +0100

    filter and pagination

--- fsa_approved_premises/frontend/views.py
+++ fsa_approved_premises/frontend/views.py
@@ -1,7 +1,9 @@
 from flask import (
     current_app,
     Blueprint,
-    render_template
+    render_template,
+    request,
+    jsonify
 )
 
 import requests
@@ -10,10 +12,20 @@ frontend = Blueprint('frontend', __name__, template_folder='templates')
 
 headers = {"Content-type": "application/json"}
 
+
 @frontend.route('/')
 def index():
     premises_url = current_app.config['PREMISES_REGISTER']
-    url = "%s/search?_query=&_representation=json" % premises_url
+    url = "%s/search?_representation=json" % premises_url
+    resp = requests.get(url, headers=headers)
+    return render_template('index.html', data=resp.json())
+
+@frontend.route('/search')
+def search():
+    query = request.args.get('query', '')
+    page = request.args.get('page', 0)
+    premises_url = current_app.config['PREMISES_REGISTER']
+    url = "%s/search?_query=%s&_page=%s&_representation=json" % (premises_url, query, page)
     resp = requests.get(url, headers=headers)
     current_app.logger.info(resp.json())
-    return render_template('index.html', data=resp.json())
+    return jsonify(resp.json())
--- fsa_approved_premises/static/javascripts/app.js
+++ fsa_approved_premises/static/javascripts/app.js
@@ -1,7 +1,6 @@
 var fetchRecords = function(input) {
     var searchTerm = input.value;
     if(searchTerm.length >= 3) {
-        console.log('do a search');
         doSearch(searchTerm)
     } else {
         reload();
@@ -11,13 +10,12 @@ var fetchRecords = function(input) {
 var doSearch = function(searchTerm) {
     $.ajax({
       type: 'GET',
-      url: "http://premises.openregister.org/search?_representation=json&_query="+searchTerm,
+      url: "/search?query="+searchTerm,
       success: function(data) {
         $('#premises-list').empty();
         $('#count').text(data.entries.length);
         $.each(data.entries, function(index, item){
-            console.log(item.entry);
-            var item = $('<li>'+ item.entry.name +'</li>');
+            var item = $('<li><a href="#">'+ item.entry.name +'</a></li>');
             $('#premises-list').append(item);
         });
       },
@@ -28,16 +26,14 @@ var doSearch = function(searchTerm) {
 };
 
 var reload = function() {
-    console.log('reload all');
     $.ajax({
       type: 'GET',
-      url: "http://premises.openregister.org/search?_representation=json",
+      url: "search",
       success: function(data) {
         $('#premises-list').empty();
         $('#count').text(data.meta.total_entries);
-        $.each(data.entries, function(index, item){
-            console.log(item.entry);
-            var item = $('<li>'+ item.entry.name +'</li>');
+        $.each(data.entries, function(index, item) {
+            var item = $('<li><a href="#">'+ item.entry.name +'</a></li>');
             $('#premises-list').append(item);
         });
       },
@@ -47,9 +43,32 @@ var reload = function() {
     });
 };
 
+var pager = function(event) {
+  event.preventDefault();
+  var searchTerm = $('#search')[0].value,
+    page = $('#page-number').text();
+  $.ajax({
+    type: 'GET',
+    url: "/search?query="+searchTerm+"&page="+page,
+    success: function(data) {
+      $('#premises-list').empty();
+      $('#count').text(data.meta.total_entries);
+      $('#page-number').text(data.meta.page+1);
+      $.each(data.entries, function(index, item) {
+          var item = $('<li><a href="#">'+ item.entry.name +'</a></li>');
+          $('#premises-list').append(item);
+      });
+    },
+    error: function(){
+      console.log("error");
+    }
+  });
+};
+
 
 $(document).ready(function() {
     $('#search').on('keyup', function(event) {
-        fetchRecords(event.currentTarget);
+      fetchRecords(event.currentTarget);
     });
+    $('#pager').on('click', pager);
 });
